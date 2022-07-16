ec206c78d3496b620f0c973d7d8bd7dbd2a845e0
bt21102036 <98831641+bt21102036@users.noreply.github.com>
2022-07-16 19:52:17 +0530

    Add files via upload

---
+++ output assignment 7.py
@@ -0,0 +1,47 @@
+#OUTPUT OF QUESTION 4
+
+Enter number of inputs:6
+Enter number:1
+Enter number:2
+Enter number:2
+Enter number:4
+Enter number:8
+Enter number:12
+[1, 2, 4, 8, 12]
+PS D:\Python> python -u "d:\Python\tempCodeRunnerFile.py"
+
+#OUTPUT OF QUESTION 5
+
+Enter size of list: 10
+Enter marks: 24
+Enter marks: 45
+Enter marks: 56
+Enter marks: 10
+Enter marks: 51
+Enter marks: 60
+Enter marks: 67
+Enter marks: 87
+Enter marks: 59
+Enter marks: 15
+[10, 15, 24, 45, 51, 56, 59, 60, 67, 87]
+Enter size of array: 6
+Enter element 1
+Enter element 2
+Enter element 4
+Enter element 4
+Enter element 5
+Enter element 6
+[1, 2, 4, 4, 5, 6]
+Enter element to be searched and counted: 4
+Element is present at index 2
+Number of occurrences of element is 2
+
+#OUTPUT OF QUESTION 6
+Enter number of inputs:5
+Enter number:1
+Enter number:2
+Enter number:3
+Enter number:2
+Enter number:3
+[1, 2, 3]
+PS D:\Python> 
